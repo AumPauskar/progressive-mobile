30ed4f21a05025e8b7887c0725bd24e54d6ab7f5
Aum Pauskar <aumpauskar111@gmail.com>
2024-01-06 17:49:18 +0530

    image formatting added in

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,7 +22,8 @@ class _MyAppState extends State<MyApp> {
   final _employeeNameController = TextEditingController();
   final _customerNameController = TextEditingController();
   String _generatorModel = '';
-  XFile? _image;
+  XFile? _image1;
+  XFile? _image2;
 
   @override
   Widget build(BuildContext context) {
@@ -39,11 +41,19 @@ class _MyAppState extends State<MyApp> {
                 // ... other form fields ...
                 ElevatedButton(
                   onPressed: () async {
-                    _image = await ImagePicker()
+                    _image1 = await ImagePicker()
                         .pickImage(source: ImageSource.gallery);
                     setState(() {});
                   },
-                  child: const Text('Pick Image'),
+                  child: const Text('Pick Image 1'),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    _image2 = await ImagePicker()
+                        .pickImage(source: ImageSource.gallery);
+                    setState(() {});
+                  },
+                  child: const Text('Pick Image 2'),
                 ),
                 ElevatedButton(
                   onPressed: () {
@@ -65,20 +75,48 @@ class _MyAppState extends State<MyApp> {
   Future<void> generatePdf(BuildContext context) async {
     final pdf = pw.Document();
 
-    final imageProvider = pw.MemoryImage(
-      await _image!.readAsBytes(),
+    final imageProvider1 = pw.MemoryImage(
+      await _image1!.readAsBytes(),
+    );
+
+    final imageProvider2 = pw.MemoryImage(
+      await _image2!.readAsBytes(),
     );
 
     pdf.addPage(
       pw.Page(
-        build: (pw.Context context) => pw.Center(
-          child: pw.Column(
-            mainAxisAlignment: pw.MainAxisAlignment.center,
-            children: [
-              // ... other text fields ...
-              pw.Image(imageProvider),
-            ],
-          ),
+        build: (pw.Context context) => pw.Row(
+          crossAxisAlignment: pw.CrossAxisAlignment.start,
+          children: [
+            // Left Section
+            pw.Container(
+              child: pw.Column(
+                mainAxisAlignment: pw.MainAxisAlignment.center,
+                crossAxisAlignment: pw.CrossAxisAlignment.start,
+                children: [
+                  pw.Text('Employee Name',
+                      style: const pw.TextStyle(
+                          fontSize: 20)), // Increase the fontSize here
+                  pw.Text('Customer Name',
+                      style: const pw.TextStyle(
+                          fontSize: 20)), // Increase the fontSize here
+                  // Resized Image
+                  pw.SizedBox(height: 20),
+                  pw.Image(imageProvider1, width: 200, height: 200),
+                ],
+              ),
+            ),
+            // Right Section
+            pw.Container(
+              child: pw.Column(
+                mainAxisAlignment: pw.MainAxisAlignment.center,
+                crossAxisAlignment: pw.CrossAxisAlignment.start,
+                children: [
+                  pw.Image(imageProvider2, width: 200, height: 200),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
